385274c111eac1089c020e69faf309ba7a959a74
Itodo Emmanuel Itodo <54606115+itodoemmanuel@users.noreply.github.com>
2023-07-31 07:18:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const nodemon = reqiuire("nodemon");
 const express = require('express');
+const body-parser =require('body-parser');
+const app = express();
+app.use(body-parser.urlencoded({extende: true}));
+
+app.get ("/", function (req, res){
+  var distance = Number(req.body.n1);
+  var time = Number(req.body.n2);
+  var speed = distance / time;
+  res.send ('Your current speed is: '+ result);
+});
+app.listen(3000);
